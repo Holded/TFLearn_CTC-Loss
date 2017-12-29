759b7bf260461665cc41c177ce45d102b6ec076b
Holded <qintsinghua@qq.com>
2017-12-29 17:20:11 +0800

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,9 +1,12 @@
 from __future__ import division, print_function, absolute_import
 import tflearn
 import speech_data
+import numpy as np
 import tensorflow as tf
 from tensorflow.python.ops import ctc_ops as ctc
 from tflearn.layers.recurrent import BasicLSTMCell
+
+from tensorflow.python.framework import sparse_tensor
 
 config = tf.ConfigProto()
 config.gpu_options.allow_growth = True
@@ -23,15 +26,20 @@ classes = 10  # digits
 batch = speech_data.mfcc_batch_generator(total_size, height)
 X, Y = next(batch)
 
-indices, values, shape = speech_data.convert_to_sparse(Y)
-Y = tf.SparseTensorValue(indices,values,shape)
+indices = tf.where(tf.not_equal(tf.cast(np.array(Y),tf.float32),0))
+Y = tf.SparseTensor(indices, values=tf.gather_nd(Y,indices)-1, dense_shape=tf.cast(tf.shape(Y),tf.int64))
+
+if isinstance(Y, sparse_tensor.SparseTensor):
+  print("Good Job!")
+else:
+  print("This is not a SparseTensor_ from demo.py")
 
 # Network building
 net = tflearn.input_data([None, width, height])
 net = tflearn.lstm(net, 50, dropout=0.8, return_seq=True)
 net = tflearn.lstm(net, 50, dropout=0.8, return_seq=True)
 net = tflearn.lstm(net, 50, dropout=0.8)
-net = tflearn.fully_connected(net, classes, activation='softmax')
+net = tflearn.fully_connected(net, classes)#, activation='softmax')
 
 adam= tflearn.optimizers.adam(learning_rate=0.001,beta1=0.99,beta2=0.999)
 net = tflearn.regression(net, optimizer=adam, loss='ctc_loss')
